f2612f1b94424cc6b1895f3cf17ad9f797c2fbc7
Bernardino Silva <bernardinoosilvaa@gmail.com>
2022-12-09 09:57:45 -0300

    atualizando o .gitignore

--- index.php
+++ index.php
@@ -6,10 +6,9 @@ require_once __DIR__ . "/vendor/autoload.php";
 
 $html = new Html;
 
-$link = $html->a("Meu site", [
-    "href" => "https://bernardinoslv.com"
+$link = $html->a("Link", [
+    "href" => "https://www.bernardinoslv.com",
+    "class" => "btn btn-sm btn-info"
 ]);
 
 echo $link;
-
-echo 
